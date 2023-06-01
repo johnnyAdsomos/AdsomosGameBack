c7f13285948aeeebd6dca3c86182ada856c2dadc
Thales Minoru Sato <thalesms2@outlook.com>
2023-06-01 18:48:07 -0300

    feat: serve static assets

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 import app from './src/app'
+import express from 'express';
+import path from 'path'
 
 const port = process.env.PORT || 4000;
+
+app.use('/static', express.static(path.join(__dirname + '/public')));
+
 app.listen(port, () =>
     console.log(`🚀 Server ready at: http://localhost:${port}`)
 );
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import Path from '../path'
+import path from 'path'
 import fs from 'fs'
 
 import { Router, Request, Response } from 'express';
@@ -12,16 +13,16 @@ route.get('/', (req: Request, res: Response) => {
     res.json({ message: 'Hello World!' })
 })
 
-route.get('/assets/centered/:champion', (req, res) => {
-    const champion = req.params.champion
-    const options = {
-        root: Path
-    };
-    console.log(options)
-    const fileName = `/public/images/centered/${champion}.jpg`
-    res.sendFile(fileName, options, (err) => {})
-    
-})
+// route.get('/assets/centered/:champion', (req, res) => {
+//     const champion = req.params.champion
+//     const options = {
+//         root: Path
+//     };
+//     console.log(options)
+//     const fileName = `/public/images/centered/${champion}.jpg`
+//     res.sendFile(fileName, options, (err) => {})
+// })
+
 
 app.use(route)
 
