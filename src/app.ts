eb5339a1cabfccd60e62b0b112a35adcdc44ad03
Thales Minoru Sato <thalesms2@outlook.com>
2023-06-01 18:59:13 -0300

    feat: remove skins

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,4 @@
 import express from 'express';
-import Path from '../path'
-import path from 'path'
-import fs from 'fs'
 
 import { Router, Request, Response } from 'express';
 
@@ -12,17 +9,6 @@ app.use(express.json());
 route.get('/', (req: Request, res: Response) => {
     res.json({ message: 'Hello World!' })
 })
-
-// route.get('/assets/centered/:champion', (req, res) => {
-//     const champion = req.params.champion
-//     const options = {
-//         root: Path
-//     };
-//     console.log(options)
-//     const fileName = `/public/images/centered/${champion}.jpg`
-//     res.sendFile(fileName, options, (err) => {})
-// })
-
 
 app.use(route)
 
